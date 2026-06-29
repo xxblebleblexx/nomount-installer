005b7134d999c10a204c80704fadc4f4796a55c1
xxblebleblexx <cicaknempel2@gmail.com>
2026-06-30 00:20:31 +0700

    upload nomount installer

---
+++ nomount.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+KERNEL_VERSION="$1"
+
+git clone -b master --depth=1 https://github.com/maxsteeel/nomount.git;wait
+cp nomount/kernel/src/nomount.c fs/
+cp nomount/kernel/src/nomount.h fs/
+
+if [ "$KERNEL_VERSION" = "4.9" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "4.14" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "4.19" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "5.4" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "5.10" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "5.15" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "6.1" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "6.6" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "6.12" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
+
+if [ "$KERNEL_VERSION" = "6.18" ]; then
+cp nomount/kernel/patches/nomount_${KERNEL_VERSION}_kernel_integration.patch .
+patch -p1 < nomount_${KERNEL_VERSION}_kernel_integration.patch
+fi
